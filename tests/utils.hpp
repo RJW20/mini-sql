9d3fdf19c1084973c385bae84f3e998ac95454a6
RJW20 <robertjwatts84@outlook.com>
2025-07-08 10:47:29 +0100

    Rework how tests/utils creates Varchar's vs arithmetic types

--- tests/utils.hpp
+++ tests/utils.hpp
@@ -8,18 +8,30 @@
 
 #include "varchar.hpp"
 
+template <typename T>
+struct default_size {
+    static_assert(std::is_arithmetic_v<T>);
+    static constexpr std::size_t value = sizeof(T);
+};
+
 inline constexpr std::size_t TEST_VARCHAR_SIZE = 10;
+template <>
+struct default_size<minisql::Varchar> {
+    static constexpr std::size_t value = TEST_VARCHAR_SIZE; 
+};
 
 template <typename T>
-T generate_new(std::size_t length = TEST_VARCHAR_SIZE) {
+T generate_new(std::size_t size = default_size<T>::value) {
+    static_assert(std::is_arithmetic_v<T>);
     static int seed = 0;
-    if constexpr (std::is_same_v<T, minisql::Varchar>) {
-        std::string underlying = "v" + std::to_string(seed++);
-        return minisql::Varchar(underlying.c_str(), length);
-    }
-    else {
-        return seed++ + 0.5;
-    }
+    return seed++ + 0.5;
+}
+
+template <>
+inline minisql::Varchar generate_new<minisql::Varchar>(std::size_t size) {
+    static int seed = 0;
+    std::string underlying = "v" + std::to_string(seed++);
+    return minisql::Varchar(underlying.c_str(), size);
 }
 
 #endif // UTILITIES_HPP
