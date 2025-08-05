fb9efb625d1ac654cead654904ed43f623681aba
RJW20 <robertjwatts84@outlook.com>
2025-08-05 12:01:16 +0100

    Remove use of minisql namespace within test_node.hpp

--- tests/bplus_tree/test_node.hpp
+++ tests/bplus_tree/test_node.hpp
@@ -1,23 +1,23 @@
 #ifndef TEST_NODE_HPP
 #define TEST_NODE_HPP
 
-#include "bplus_tree/node.hpp"
-
 #include <cstddef>
 #include <utility>
 
+#include "bplus_tree/node.hpp"
 #include "frame_manager/cache/frame_view.hpp"
 #include "frame_manager/disk_manager/page_id_t.hpp"
 #include "headers.hpp"
 
-using namespace minisql;
-
-// A Node with each key taking up the entirety of a slot
-class TestNode : public Node {
+// A Node with each key taking up the entirety of a slot.
+class TestNode : public minisql::Node {
 public:
-    TestNode(FrameView&& fv, key_size_t key_size, page_id_t parent)
-        : Node{std::move(fv), Magic{0xFF}, key_size, key_size, parent} {}
-    using Node::Node;
+    TestNode(
+        minisql::FrameView&& fv, key_size_t key_size, minisql::page_id_t parent
+    ) : minisql::Node{
+            std::move(fv), minisql::Magic{0xFF}, key_size, key_size, parent
+        } {}
+    using minisql::Node::Node;
 
     bool is_leaf() const override final { return false; }
 
@@ -27,15 +27,15 @@ public:
         set_key(slot, key);
     }
 
-    using Node::shift;
+    using minisql::Node::shift;
 
-    using Node::assert_compatibility;
-    using Node::splice_back_to_front;
-    using Node::splice_front_to_back;
+    using minisql::Node::assert_compatibility;
+    using minisql::Node::splice_back_to_front;
+    using minisql::Node::splice_front_to_back;
 
 private:
     std::size_t header_size() const override final {
-        return NodeHeader::SIZE;
+        return minisql::NodeHeader::SIZE;
     }
 
     size_t min_size() const override final { return 0; }
