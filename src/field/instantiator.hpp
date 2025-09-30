fcc4aa17282ec1979f3eba7a8d16d0305b28be3c
RJW20 <robertjwatts84@outlook.com>
2025-09-30 14:54:04 +0100

    Push new InstantiateForField<FuncWrapper> struct that BPlusTree components use for explicit instantiation for all types in a Field

---
+++ src/field/instantiator.hpp
@@ -0,0 +1,34 @@
+#ifndef MINISQL_INSTANTIATOR_HPP
+#define MINISQL_INSTANTIATOR_HPP
+
+#include <variant>
+
+#include "field/field.hpp"
+
+namespace minisql {
+
+namespace {
+
+template <template <typename> typename FuncWrapper, typename Variant>
+struct InstantiateForVariant;
+
+template <template <typename> typename FuncWrapper, typename... Ts>
+struct InstantiateForVariant<FuncWrapper, std::variant<Ts...>> {
+    static void instantiate() {
+        (FuncWrapper<Ts>::instantiate(), ...);
+    }
+};
+
+} // namespace
+
+/* Struct for instantiating a templated function or method for all types in
+ * Field.
+ * Call InstantiateForField<FuncWrapper>::instantiate() where
+ * FuncWrapper::instantiate calls the desired function with correct signature.
+ */
+template <template <typename> typename FuncWrapper>
+using InstantiateForField = InstantiateForVariant<FuncWrapper, Field>;
+
+} // namespace minisql
+
+#endif // MINISQL_INSTANTIATOR_HPP
