4643c01b82d259b9a983d2f1e0bc0d9e5ca16a75
RJW20 <robertjwatts84@outlook.com>
2025-10-01 21:13:15 +0100

    Write Engine class for managing databases and executing sql on them

---
+++ src/engine/engine.cpp
@@ -0,0 +1,138 @@
+#include "engine/engine.hpp"
+
+#include <cstddef>
+#include <filesystem>
+#include <memory>
+#include <variant>
+#include <utility>
+#include <string>
+#include <string_view>
+
+#include "engine/database_handle.hpp"
+#include "database.hpp"
+#include "parser/ast.hpp"
+#include "parser/parser.hpp"
+#include "engine/master_table.hpp"
+#include "row/schema.hpp"
+#include "row_set/row_set.hpp"
+#include "row/row.hpp"
+#include "validator/query.hpp"
+#include "validator/validator.hpp"
+#include "field/varchar.hpp"
+
+namespace minisql {
+
+/* Return a DatabaseHandle providing access to the database with given path.
+ * If the database is already open then a new DatabaseHandle is created and
+ * returned.
+ * Otherwise the database is opened and stored, the master table and its
+ * contents is added to the database's catalog and then a new DatabaseHandle
+ * is created and returned. */
+DatabaseHandle Engine::open_database(const std::filesystem::path& path) {
+    auto it = dbs_.find(path);
+    if (it != dbs_.end()) return DatabaseHandle{*this, it->second, path};
+    auto db = std::make_shared<Database>(path);
+    auto create_ast = std::get<parser::CreateAST>(
+        parser::parse(master_table::build_create_statement())
+    );
+    db->add_table(
+        master_table::NAME,
+        Schema::create(
+            create_ast.columns, create_ast.types, create_ast.sizes,
+            *(create_ast.primary)
+        ),
+        db->master_root()
+    );
+    RowSet tables = query(master_table::build_select_statement(), *db);
+    for (Row& table_info : tables) {
+        parser::AST ast = parser::parse(std::get<Varchar>(
+            table_info[master_table::columns::SQL.name]
+        ).data());
+        auto c_query = std::get<validator::CreateQuery>(
+            validator::validate(ast, *db, false)
+        );
+        db->add_table(
+            std::get<Varchar>(
+                table_info[master_table::columns::TABLE_NAME.name]
+            ).data(),
+            Schema::create(
+                c_query.columns, c_query.types, c_query.sizes, c_query.primary
+            ),
+            std::get<int>(table_info[master_table::columns::ROOT.name]),
+            std::get<int>(table_info[master_table::columns::NEXT_ROWID.name])
+        );
+    }
+    dbs_[path] = db;
+    return DatabaseHandle(*this, std::move(db), path);
+}
+
+/* Close the database with given path if there are no Connections using it.
+ * Updates its master table and master_root before closing. */
+void Engine::release_database(const std::filesystem::path& path) {
+    auto it = dbs_.find(path);
+    if (it == dbs_.end()) return;
+    auto db = it->second;
+    if (db.use_count() > 2) return;
+    RowSet tables = query(
+        master_table::build_select_statement(
+            {master_table::columns::TABLE_NAME}
+        ),
+        *db
+    );
+    for (Row& table_info : tables) {
+        const std::string& table_name = std::get<Varchar>(
+            table_info[master_table::columns::TABLE_NAME.name]
+        ).data();
+        const Table* table = db->find_table(table_name);
+        exec(
+            master_table::build_update_statement(
+                {
+                    {
+                        master_table::columns::ROOT.name,
+                        std::to_string(table->bp_tree->root())
+                    },
+                    {
+                        master_table::columns::NEXT_ROWID.name,
+                        std::to_string(table->next_rowid)
+                    }
+                }, table_name
+            ), *db, true
+        );
+    }
+    db->set_master_root(db->find_table(master_table::NAME)->bp_tree->root());
+    dbs_.erase(it);
+}
+
+// Execute the given sql on the given database.
+std::size_t Engine::exec(
+    std::string_view sql, Database& db, bool master_enabled
+) {
+    parser::AST ast = parser::parse(sql);
+    validator::Query query = validator::validate(ast, db, master_enabled);
+    planner::Plan plan = planner::plan(query, db);
+    plan->open();
+    while (plan->next());
+    plan->close();
+    if (std::holds_alternative<validator::CreateQuery>(query)) {
+        std::string table_name = std::get<validator::CreateQuery>(query).table;
+        const Table* table = db.find_table(table_name);
+        exec(
+            master_table::build_insert_statement(
+                table_name, sql.data(), table->bp_tree->root(),
+                table->next_rowid
+            ), db, true
+        );
+    }
+    return plan->count();
+}
+
+/* Return a RowSet containing all rows output when executing the given sql on
+ * the given database. */
+RowSet Engine::query(std::string_view sql, Database& db) {
+    parser::AST ast = parser::parse(sql);
+    validator::Query query = validator::validate(ast, db, false);
+    planner::Plan plan = planner::plan(query, db);
+    return RowSet{std::move(plan)};
+}
+
+} // namespace minisql
---
+++ src/engine/engine.hpp
@@ -0,0 +1,57 @@
+#ifndef MINISQL_ENGINE_HPP
+#define MINISQL_ENGINE_HPP
+
+#include <cstddef>
+#include <filesystem>
+#include <string>
+#include <string_view>
+#include <memory>
+
+#include "engine/database_handle.hpp"
+#include "database.hpp"
+#include "row_set/row_set.hpp"
+
+namespace minisql {
+
+/* Custom hash for std::filesystem::path that ensures normalisation around .
+ * and .. */
+struct PathHash {
+    std::size_t operator()(const std::filesystem::path& p) const {
+        return std::hash<std::string>()(
+            std::filesystem::weakly_canonical(p).string()
+        );
+    }
+};
+
+/* Custom equality for std::filesystem::path that ensures normalisation around
+ * . and .. */
+struct PathEqual {
+    bool operator()(
+        const std::filesystem::path& lhs, const std::filesystem::path& rhs
+    ) const {
+        return std::filesystem::weakly_canonical(lhs) ==
+            std::filesystem::weakly_canonical(rhs);
+    }
+};
+
+/* Engine
+ * Manages access to Databases and handles execution of sql on them. */
+class Engine {
+public:
+    DatabaseHandle open_database(const std::filesystem::path& path);
+    void release_database(const std::filesystem::path& path);
+
+    std::size_t exec(
+        std::string_view sql, Database& db, bool master_enabled = false
+    );
+    RowSet query(std::string_view sql, Database& db);
+
+private:
+    std::unordered_map<
+        std::filesystem::path, std::shared_ptr<Database>, PathHash, PathEqual
+    > dbs_;
+};
+
+} // namespace minisql
+
+#endif // MINISQL_ENGINE_HPP
