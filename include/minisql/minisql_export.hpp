8ea7061aaaab25a2870ae475c8ceb6a6fe486882
RJW20 <robertjwatts84@outlook.com>
2026-01-08 14:18:56 +0000

    Fix CMakeLists.txt and minisql_exports.hpp to distinguish static/shared builds correctly

--- include/minisql/minisql_export.hpp
+++ include/minisql/minisql_export.hpp
@@ -1,14 +1,21 @@
 #ifndef MINISQL_EXPORT_HPP
 #define MINISQL_EXPORT_HPP
 
-#ifdef _WIN32
-  #ifdef MINISQL_EXPORTS
-    #define MINISQL_API __declspec(dllexport)
-  #else
-    #define MINISQL_API __declspec(dllimport)
-  #endif
+// For Windows (MSVC, MinGW, Cygwin)
+#if defined(_WIN32) || defined(__CYGWIN__)
+    // Static build
+    #if defined(MINISQL_STATIC)
+        #define MINISQL_API
+    // Shared build
+    #elif defined(MINISQL_EXPORTS)
+        #define MINISQL_API __declspec(dllexport)
+    // Using DLL
+    #else
+        #define MINISQL_API __declspec(dllimport)
+    #endif
 #else
-  #define MINISQL_API
+    // Linux / macOS
+    #define MINISQL_API
 #endif
 
 #endif // MINISQL_EXPORT_HPP
