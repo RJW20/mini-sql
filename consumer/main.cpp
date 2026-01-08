6df19ca702585a1b90b9a6d802d5d1af3395d81d
RJW20 <robertjwatts84@outlook.com>
2026-01-08 14:19:26 +0000

    Add scope to consumer main program to ensure deletion

--- consumer/main.cpp
+++ consumer/main.cpp
@@ -6,9 +6,11 @@ namespace fs = std::filesystem;
 
 int main() {
     const fs::path db_path = "test.db";
-    minisql::Connection conn(db_path);
-    conn.exec("CREATE TABLE t (int INT, real REAL, text TEXT(10));");
-    conn.exec("DROP TABLE t;");
+    {
+        minisql::Connection conn(db_path);
+        conn.exec("CREATE TABLE t (int INT, real REAL, text TEXT(10));");
+        conn.exec("DROP TABLE t;");
+    }
     fs::remove(db_path);
     return 0;
 }
