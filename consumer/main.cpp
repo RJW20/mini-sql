f516b6123f0955c965e76515b757ade6f5e6941e
RJW20 <robertjwatts84@outlook.com>
2026-01-08 12:26:14 +0000

    Add small consumer for library linking testing

---
+++ consumer/main.cpp
@@ -0,0 +1,14 @@
+#include <filesystem>
+
+#include <minisql/connection.hpp>
+
+namespace fs = std::filesystem;
+
+int main() {
+    const fs::path db_path = "test.db";
+    minisql::Connection conn(db_path);
+    conn.exec("CREATE TABLE t (int INT, real REAL, text TEXT(10));");
+    conn.exec("DROP TABLE t;");
+    fs::remove(db_path);
+    return 0;
+}
